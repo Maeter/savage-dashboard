88704e73c72931cf8ed0f442a80a7f02daccd5be
Luis Miguel Fernández <luis.fernandez@elementsinteractive.es>
2018-06-08 17:06:38 +0200

    Added tests for dice.js functions Todo: investigate how to avoid using exports to correctly mock fn on tests

--- src/services/dice.js
+++ src/services/dice.js
@@ -1,11 +1,15 @@
-export const roll = die => {
-  const result = Math.floor(Math.random() * Math.floor(die)) + 1;
-  return result === die ? result + roll(die) : result;
-}
+
+export const naturalRoll = die =>
+  Math.floor(Math.random() * Math.floor(die)) + 1;
+
+export const roll = (die, rollingFn = exports.naturalRoll) => {
+  const rolledVal = rollingFn(die);
+  return rolledVal === die ? rolledVal + roll(die, rollingFn) : rolledVal;
+};
 
 export const traitCheck = (die = 4) => {
-  const dx = roll(die);
-	const d6 = roll(6);
+  const dx = exports.roll(die);
+	const d6 = exports.roll(6);
 	const max = Math.max(dx, d6);
 	return max;
-}
+};
---
+++ src/services/dice.test.js
@@ -0,0 +1,31 @@
+import * as dice from './dice';
+
+describe('roll fn', () => {
+  it('returns expected values', () => {
+    function* rollerFnGenerator() {
+      yield 1;
+      yield 2;
+      yield 3;
+      yield 4;
+      yield 1; // <- This one is the exploded value of teh last roll
+    };
+    const rollerFn = rollerFnGenerator();
+    const roll = () => rollerFn.next().value;
+    expect(dice.roll(4, roll)).toBe(1);
+    expect(dice.roll(4, roll)).toBe(2);
+    expect(dice.roll(4, roll)).toBe(3);
+    // Rolls 4, explodes and then rolls 1
+    expect(dice.roll(4, roll)).toBe(5);
+  })
+});
+
+describe('traitCheck fn', () => {
+  it('returns expected values', () => {
+    // We assume each roll will come up the highest possible value
+    dice.roll = jest.fn(x => x);
+    // Rolled values below 6 (included), return 6,
+    // otherwise the other value
+    for(let i = 1; i<13; i++)
+      expect(dice.traitCheck(i)).toBe(i < 6 ? 6 : i);
+  })
+});
