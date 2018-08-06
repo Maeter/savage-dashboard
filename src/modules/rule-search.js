1133da862a202cb480bd2d87cb02415926923a2b
Luis Miguel Fernández <lmfernandezb@gmail.com>
2018-08-06 21:25:18 +0200

    preliminary searcher working

--- src/modules/rule-search.js
+++ src/modules/rule-search.js
@@ -3,8 +3,9 @@ import generateKey from 'shortid';
 
 import filter from 'lodash/filter';
 import throttle from 'lodash/throttle';
-import rules from '../assets/rules';
 import swdJson from '../assets/swd_json.json';
+
+import Input from '../components/input';
 
 class RuleSearch extends Component {
   constructor(props) {
@@ -17,6 +18,7 @@ class RuleSearch extends Component {
         tables: [],
       },
       results: [],
+      selectedResult: null,
     };
   }
 
@@ -25,57 +27,63 @@ class RuleSearch extends Component {
       .sort((a, b) => a.pageId - b.pageId);
     const orderedTexts = orderedpages
       .reduce((acc, page) => [...acc, ...page.texts], []);
-    const titles = orderedTexts.filter((text) => text.fontSize === 15);
+    let prevIndex = 0;
+    const titles = orderedTexts.filter(
+      (text, index) => {
+        const isTitle = text.fontSize === 15 // Remove subtitles
+          && isNaN(text.text) // Remove page numbers
+          && text.text.indexOf('....') === -1; // Remove index entries
+        if(isTitle) {
+          orderedTexts[prevIndex].indexEnd = index;
+          orderedTexts[index].indexStart = index;
+          prevIndex = index;
+        }
+        return isTitle;
+    });
     console.log(titles);
+    this.setState({ texts: orderedTexts, titles });
   }
 
   updateTerm = () => {
-    const val = this.input.value;
-    const rx = new RegExp(val);
-    const results = filter(
-      rules,
-      (item) => item.keywords.reduce((acc, i) => acc || rx.test(i), false),
-    ) || this.state.results; // Keep the previous selection if no match
-    this.setState({
-      term: val,
-      selected: results && (results[0] || {}),
-      results,
-    });
+    if(this.input.value.length >2){
+      const val = this.input.value.toLowerCase();
+      const rx = new RegExp(val);
+      const results = filter(
+        this.state.titles,
+        (item) => rx.test(item.text.toLowerCase()),
+      ) || this.state.results; // Keep the previous selection if no match
+      this.setState({
+        term: val,
+        selected: results && (results[0] || {}),
+        results,
+      });
+    }
   };
 
+  displayRule = (item) => {
+    const { texts } = this.state;
+    const range = texts.slice(item.indexStart + 1, item.indexEnd).map(i => i.text);
+    this.setState({
+      selectedResult: range.join(''),
+    });
+  }
+
   throttledUpdateTerm = throttle(this.updateTerm, 800);
-
-  renderTexts = texts =>
-    texts && texts.map((t, i) => (<p key={generateKey()}>{t}</p>));
-
-  renderTables = tables => (
-    <table>
-      <tbody>
-        {tables && tables.map((col, i) => (
-          <tr key={generateKey()}>
-            {col.map((val, j) => (<td key={generateKey()}>{val}</td>))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
 
   render() {
     const { results, selected } = this.state;
     return (
       <div className={'rule-search'}>
         <h3>Search rules</h3>
-        <input type="text" onChange={this.throttledUpdateTerm} ref={x => this.input = x}/>
+        <Input type="text" onChange={this.throttledUpdateTerm} innerRef={x => this.input = x}/>
         <br/>
-        {results.map((res, i) => <span key={i}>{res.id}, </span>)}
-        <br/>
+        {results.map((res, i) => <span key={i} onClick={() => this.displayRule(res)}>{res.text} </span>)}
         <br/>
         {selected.keywords && selected.keywords.map((k, i) => <span key={i}>{k}, </span>)}
         <br/>
         <br/>
-        {this.renderTexts(this.state.selected.texts)}
+        <p>{this.state.selectedResult}</p>
         <br/>
-        {this.renderTables(this.state.selected.tables)}
       </div>
     );
   }
