5983901475efc89b4adff1c6ff7fbf5fcc6544e8
Luis Miguel Fernández <lmfernandezb@gmail.com>
2018-08-14 22:05:18 +0200

    Added bold and more subtitles to 50Fathoms rules search

--- src/rules/50-fathoms/index.js
+++ src/rules/50-fathoms/index.js
@@ -4,7 +4,7 @@ import ruleset from './50fathoms.json';
 let prevIndex = 0;
 const titles = ruleset.filter(
   (text, index) => { // Titles on 50Fathoms (11)
-    const isTitle = text.fontSize === 11
+    const isTitle = ([11, 17].includes(text.fontSize) || text.bold === true)
       && isNaN(text.text) // Remove page numbers
       && text.text.indexOf('....') === -1; // Remove index entries
     if(isTitle) {
